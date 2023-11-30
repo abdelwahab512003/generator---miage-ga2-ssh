df127025aefbbf7f9f977eab71c5df0f1398a822
abdelwahab512003 <abdelwahababali003@gmail.com>
2023-11-30 15:36:06 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//console.log(window.document);
+//reupération de l'élément input par son id firstcolor
+const firstcolorElt = document.getElementById('firstcolor');
+//console.log(firstcolorElt);
+//reupération de l'élément input par son id secondcolor
+const secondcolorElt = document.getElementById('secendcolor');
+//console.log(ssecondcolorElt);
+
+//reupération de l'élément body
+const bodyElt = document.querySelector('body');
+console.log(bodyElt);
+ 
+//
+ firstcolorElt.addEventListener('input',function() {
+   // console.log(firstcolorElt.value);
+    bodyElt.style = "background: linear-gradient(to right,"+firstcolorElt.value+","+secondcolorElt.value+")";
+ });
+
+ secondcolorElt.addEventListener('input',function() {
+  // console.log(secondcolorElt.value);
+  bodyElt.style =  `background: linear-gradient(to right, ${firstcolorElt.value}, ${secondcolorElt.value});`
+});
